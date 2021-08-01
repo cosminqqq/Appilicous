c5f85b4a68095ca4a1b527152f59bc6bb0431255
cosminqqq <48871216+cosminqqq@users.noreply.github.com>
2021-08-01 15:35:46 +0300

    Add files via upload

---
+++ jsBig.js
@@ -0,0 +1,13 @@
+ 	$('.icon').click( function () {   
+			$('nav').toggleClass('open-nav', '0');
+		
+		var x = document.querySelector('.open-nav ul');
+		x.style.height = window.innerHeight+"px";
+
+	});
+	
+	
+	
+
+				
+				
